e8ce88e61525aa55a061d5f6efc0eb9ce9f78d05
Kavitha Kanchana <kanchanakavitha6@gmail.com>
2025-01-26 18:10:19 +0530

    add authentication functionality with Appwrite, implement global context provider, and update sign-in screen

---
+++ lib/appwrite.ts
@@ -0,0 +1,79 @@
+import { Account, Avatars, Client, OAuthProvider } from "react-native-appwrite";
+import * as Linking from "expo-linking";
+import { openAuthSessionAsync } from "expo-web-browser";
+
+export const config = {
+  Platform: "com.kavitha.restate",
+  endpoint: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ENDPOINT,
+  project: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+};
+
+export const client = new Client();
+
+client
+  .setEndpoint(config.endpoint!)
+  .setProject(config.project!)
+  .setPlatform(config.Platform!);
+
+export const avatar = new Avatars(client);
+export const account = new Account(client);
+
+export async function login() {
+  try {
+    const redirectUri = Linking.createURL("/");
+    const response = await account.createOAuth2Token(
+      OAuthProvider.Google,
+      redirectUri
+    );
+
+    if (!response) throw new Error("Failed to login");
+    const browserResult = await openAuthSessionAsync(
+      response.toString(),
+      redirectUri
+    );
+
+    if (browserResult.type !== "success") throw new Error("Failed to login");
+
+    const url = new URL(browserResult.url);
+    const secret = url.searchParams.get("secret")?.toString();
+    const userId = url.searchParams.get("userId")?.toString();
+
+    if (!secret || !userId) throw new Error("Failed to login");
+    const session = await account.createSession(userId, secret);
+
+    if (!session) throw new Error("Failed to crete session");
+
+    return true;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+}
+
+export async function logout() {
+  try {
+    await account.deleteSession("current");
+    return true;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+}
+
+export async function getCurrentUser() {
+  try {
+    const response = await account.get();
+    if (response.$id) {
+      const userAvatar = avatar.getInitials(response.name);
+
+      return {
+        ...response,
+        avatar: userAvatar,
+      };
+    }
+    return response;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
